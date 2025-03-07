d259921c55f1c10f8564f48cbfe84c6b4f0ae38c
eetann <hideharu092@gmail.com>
2025-03-07 09:24:16 +0900

    wip

--- denops/senpai/main.ts
+++ denops/senpai/main.ts
@@ -1,9 +1,16 @@
 import type { Entrypoint } from "jsr:@denops/std@^7.0.0";
+// import { weatherAgent } from "./weather.ts";
+import * as emoji from "npm:node-emoji";
 
 export const main: Entrypoint = (denops) => {
   denops.dispatcher = {
     async hello() {
-      await denops.cmd(`echo "Hello, Denops!"`);
+      console.log(emoji.emojify(`:sauropod: :heart:  npm`));
+      // await denops.cmd(`echo "Hello, Denops!"`);
+      // const response = await weatherAgent.generate([
+      //   { role: "user", content: "Tokyo" },
+      // ]);
+      // await denops.cmd(`echo "${response.text}"`);
     },
   };
 };
