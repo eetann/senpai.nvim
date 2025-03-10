ee6dc5d23d6bdc20aac9c5a79879fc51c904beba
eetann <hideharu092@gmail.com>
2025-03-10 18:11:24 +0900

    refactor: command handling
    Extracted command handling logic into a new module
    'command_register.lua'. This improves code organization
    and reusability by separating command registration and
    execution logic from the main command file.

--- lua/senpai/command.lua
+++ lua/senpai/command.lua
@@ -1,3 +1,4 @@
+local CommandResiter = require("senpai.presentation.command_register")
 ---@tag senpai-commands
 ---@toc_entry Commands
 ---@text
@@ -11,15 +12,6 @@
 ---@field impl fun(args:string[], opts: table) The comand implementation
 ---@field complete? fun(subcmd_arg_lead: string): string[] (optional) Command completions callback, taking the lead of the subcommand's arguments
 ---@private
-
-local function get_complete(subcmd_arg_lead, args)
-  return vim
-    .iter(args)
-    :filter(function(arg)
-      return arg:find(subcmd_arg_lead) ~= nil
-    end)
-    :totable()
-end
 
 ---@type table<string, Senpai.Subcommand>
 ---@private
@@ -41,7 +33,7 @@ local subcmd_tbl = {
         "Chinese",
         "Japanese(ツンデレ風)",
       }
-      return get_complete(subcmd_arg_lead, args)
+      return CommandResiter.get_complete(subcmd_arg_lead, args)
     end,
   },
   summarizeExample = {
@@ -54,45 +46,4 @@ local subcmd_tbl = {
   },
 }
 
----@param opts table :h lua-guide-commands-create
----@private
-local function execute_command(opts)
-  local fargs = opts.fargs
-  local subcmd_key = fargs[1]
-
-  local args = #fargs > 1 and vim.list_slice(fargs, 2, #fargs) or {}
-  local subcmd = subcmd_tbl[subcmd_key]
-
-  if not subcmd then
-    vim.notify("Senpai: Unknown command: " .. subcmd_key, vim.log.levels.ERROR)
-    return
-  end
-  subcmd.impl(args, opts)
-end
-
-vim.api.nvim_create_user_command("Senpai", execute_command, {
-  nargs = "+",
-  desc = "Senpai command with sub command completions",
-  complete = function(arg_lead, cmdline, _)
-    local subcmd_key, subcmd_arg_lead =
-      cmdline:match("^['<,'>]*Senpai[!]*%s(%S+)%s(.*)$")
-    if
-      subcmd_key
-      and subcmd_arg_lead
-      and subcmd_tbl[subcmd_key]
-      and subcmd_tbl[subcmd_key].complete
-    then
-      return subcmd_tbl[subcmd_key].complete(subcmd_arg_lead)
-    end
-    if cmdline:match("^['<,'>]*Senpai[!]*%s+%w*$") then
-      local subcmd_keys = vim.tbl_keys(subcmd_tbl)
-      return vim
-        .iter(subcmd_keys)
-        :filter(function(key)
-          return key:find(arg_lead) ~= nil
-        end)
-        :totable()
-    end
-  end,
-  bang = false,
-})
+CommandResiter.regist(subcmd_tbl)
