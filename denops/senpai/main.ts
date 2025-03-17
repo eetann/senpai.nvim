69aee9ba4828b86c943bd390ef35a47a0186933c
eetann <hideharu092@gmail.com>
2025-03-17 13:11:54 +0900

    refactor: Delete Migrated Files

--- denops/senpai/main.ts
+++ denops/senpai/main.ts
@@ -1,26 +1,15 @@
-import { autocmd, type Entrypoint } from "./deps.ts";
-import { weatherAgent } from "./weather.ts";
-import { generateCommitMessage } from "./presentation/generateCommitMessage.ts";
-import { summarize } from "./presentation/summary.ts";
+import { type Entrypoint, autocmd } from "./deps.ts";
 import { ChatController } from "./presentation/ChatController.ts";
+import { summarize } from "./presentation/summary.ts";
 
 export const main: Entrypoint = (denops) => {
-  denops.dispatcher = {
-    async chat(args) {
-      await new ChatController(denops, args).execute();
-    },
-    async hello() {
-      const response = await weatherAgent.generate([
-        { role: "user", content: "Tokyo" },
-      ]);
-      return response.text;
-    },
-    async generateCommitMessage(args) {
-      return await generateCommitMessage(args);
-    },
-    async summarize(args) {
-      await summarize(denops, args);
-    },
-  };
-  autocmd.emit(denops, "User", "SenpaiInitEnd", { nomodeline: true });
+	denops.dispatcher = {
+		async chat(args) {
+			await new ChatController(denops, args).execute();
+		},
+		async summarize(args) {
+			await summarize(denops, args);
+		},
+	};
+	autocmd.emit(denops, "User", "SenpaiInitEnd", { nomodeline: true });
 };
