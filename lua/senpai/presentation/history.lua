6ccaab1bb308d5b8a06ab027d2053ee9aedf2c97
eetann <hideharu092@gmail.com>
2025-03-18 17:38:35 +0900

    feat(history): add snacks.picker support

--- lua/senpai/presentation/history.lua
+++ lua/senpai/presentation/history.lua
@@ -2,9 +2,6 @@ local RequestHandler = require("senpai.presentation.shared.request_handler")
 
 local M = {}
 
--- TODO:pickerはokコールであったら使うようにする
--- local picker = require("snacks.picker")
-
 ---@param callback senpai.RequestHandler.callback_fun
 ---@return nil
 function M.get_history(callback)
@@ -17,6 +14,11 @@ end
 local function extract_directory_path(path)
   local dir_part = path:match("(.+)%-[0-9]+$")
   return vim.fn.fnamemodify(dir_part or path, ":~")
+end
+
+local function get_score(updated_date)
+  local digit = string.gsub(updated_date, "[^0-9]", "")
+  return tonumber(digit)
 end
 
 ---@class senpai.history.item
@@ -47,12 +49,42 @@ function M.select_history()
       vim.notify("[senpai] failed to get history :(", vim.log.levels.ERROR)
       return
     end
-    vim.ui.select(histories, {
-      prompt = "Select History",
-      format_item = make_item_text,
-    }, function(choice)
-      vim.print(choice)
-    end)
+    local ok, picker = pcall(require, "snacks.picker")
+    if not ok then
+      vim.ui.select(histories, {
+        prompt = "Select History",
+        format_item = make_item_text,
+      }, function(choice)
+        vim.print(choice)
+      end)
+    else
+      picker({
+        ---@return snacks.picker.Item[]
+        finder = function()
+          local items = {}
+          local i = 1
+          for _, history in pairs(histories) do
+            ---@return snacks.picker.Item
+            local item = {
+              idx = i,
+              score = get_score(history.updatedAt),
+              text = make_item_text(history),
+              preview = { text = vim.inspect(history), ft = "lua" },
+            }
+            table.insert(items, item)
+            i = i + 1
+          end
+          return items
+        end,
+        ---@type snacks.picker.Action.spec
+        confirm = function(the_picker, choice)
+          the_picker:close()
+          vim.print(choice)
+        end,
+        format = "text",
+        preview = "preview",
+      })
+    end
   end)
 end
 
