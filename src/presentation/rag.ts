9e942043202427511be42d191e736e3910167a34
eetann <hideharu092@gmail.com>
2025-03-31 09:21:25 +0900

    feat(rag): add interfaces

--- src/presentation/rag.ts
+++ src/presentation/rag.ts
@@ -13,6 +13,27 @@ const ragSchema = z.discriminatedUnion("type", [
 
 app.openapi(
 	createRoute({
+		method: "get",
+		path: "/",
+		responses: {
+			200: {
+				description: "get all RAG",
+				content: {
+					"application/json": {
+						schema: z.array(z.string()),
+					},
+				},
+			},
+		},
+	}),
+	async (c) => {
+		const indexes = await vector.listIndexes();
+		return c.json(indexes);
+	},
+);
+
+app.openapi(
+	createRoute({
 		method: "post",
 		path: "/",
 		request: {
@@ -27,7 +48,7 @@ app.openapi(
 		},
 		responses: {
 			200: {
-				description: "regist url",
+				description: "fetch url and store as vector",
 				content: {
 					"application/json": {
 						schema: z.object({
@@ -45,5 +66,36 @@ app.openapi(
 			message = await new FetchAndStoreUseCase(vector).execute(content.url);
 		}
 		return c.json({ message });
+	},
+);
+
+app.openapi(
+	createRoute({
+		method: "delete",
+		path: "/",
+		request: {
+			body: {
+				required: true,
+				content: {
+					"application/json": {
+						schema: ragSchema,
+					},
+				},
+			},
+		},
+		responses: {
+			204: {
+				description: "delete specific index from RAG",
+			},
+		},
+	}),
+	async (c) => {
+		const content = c.req.valid("json");
+		let message = "";
+		if (content.type === "url") {
+			// TODO: 要実装
+			// message = await new FetchAndStoreUseCase(vector).execute(content.url);
+		}
+		return new Response(undefined, { status: 204 });
 	},
 );
