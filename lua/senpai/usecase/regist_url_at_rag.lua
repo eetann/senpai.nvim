8e2df6ff8c1d68775c1d0aac37d409e798b24458
eetann <hideharu092@gmail.com>
2025-04-01 09:25:21 +0900

    feat(rag): WIP use cache

--- lua/senpai/usecase/regist_url_at_rag.lua
+++ lua/senpai/usecase/regist_url_at_rag.lua
@@ -1,37 +1,57 @@
 local post_rag_index = require("senpai.usecase.request.rag.post_rag_index")
 local Spinner = require("senpai.presentation.shared.spinner")
+local check_has_cache = require("senpai.usecase.request.rag.check_has_cache")
 local M = {}
 
-local function process_rag_registration(use_cache, url)
+---@alias senpai.rag.cache_strategy
+---| "use_cache"
+---| "no_cache"
+---| "ask"
+
+---@param cache_strategy senpai.rag.cache_strategy
+---@param url string
+local function process_rag_registration(cache_strategy, url)
   local spinner = Spinner.new("[senpai] Registering")
   spinner:start()
-  -- TODO: キャッシュがあるか確認
-  post_rag_index.execute({
-    type = "url",
-    url = url,
-  }, function(response)
-    if response.exit ~= 0 or response.status ~= 200 then
-      vim.notify(
-        "[senpai] Registration with RAG failed: " .. url,
-        vim.log.levels.WARN
-      )
-      return
-    end
-    local ok, message = pcall(vim.json.decode, response.body)
-    if not ok or type(message) ~= "string" then
-      message = ""
-    end
-    if message ~= "" then
-      vim.notify(
-        "[senpai] Registration with RAG failed: " .. url .. "\n" .. message,
-        vim.log.levels.WARN
-      )
-      return
-    end
-    vim.notify("[senpai] Registered with RAG: " .. url)
-  end, function()
-    spinner:stop()
-  end)
+
+  local function post()
+    post_rag_index.execute({
+      type = "url",
+      url = url,
+    }, function(response)
+      if response.exit ~= 0 or response.status ~= 200 then
+        vim.notify(
+          "[senpai] Registration with RAG failed: " .. url,
+          vim.log.levels.WARN
+        )
+        return
+      end
+      local ok, message = pcall(vim.json.decode, response.body)
+      if not ok or type(message) ~= "string" then
+        message = ""
+      end
+      if message ~= "" then
+        vim.notify(
+          "[senpai] Registration with RAG failed: " .. url .. "\n" .. message,
+          vim.log.levels.WARN
+        )
+        return
+      end
+      vim.notify("[senpai] Registered with RAG: " .. url)
+    end, function()
+      spinner:stop()
+    end)
+  end
+
+  if cache_strategy == "no_cache" then
+    post()
+    return
+  end
+  local has_cache = check_has_cache.execute(url)
+  if has_cache then
+    -- TODO:
+  end
+  post()
 end
 
 ---@param use_cache boolean
---
+++ lua/senpai/usecase/request/rag/check_has_cache.lua
@@ -0,0 +1,23 @@
+local RequestHandler = require("senpai.usecase.request.request_handler")
+
+local M = {}
+
+---@param source string
+---@return boolean # check if source has cache
+function M.execute(source)
+  local response = RequestHandler.request_without_callback({
+    method = "post",
+    route = "/rag",
+    body = { source = source },
+  })
+  if response.exit ~= 0 or response.status ~= 200 then
+    return false
+  end
+  local ok, body = pcall(vim.json.decode, response.body)
+  if not ok or type(body) ~= "table" then
+    return false
+  end
+  return body.hasCache or false
+end
+
+return M
