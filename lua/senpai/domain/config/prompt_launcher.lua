ac1176961dc469e56c1e513e140de448c5c8d5b9
eetann <hideharu092@gmail.com>
2025-06-05 10:16:37 +0900

    docs: add type

--- lua/senpai/domain/config/prompt_launcher.lua
+++ lua/senpai/domain/config/prompt_launcher.lua
@@ -1,5 +1,6 @@
 local M = {}
 
+---@doc.type
 ---@class senpai.Config.PromptLauncher.launcher
 ---@field provider? senpai.Config.provider.name|senpai.Config.provider
 ---@field system? string|fun():string
@@ -8,8 +9,10 @@ local M = {}
 ---@field condition? fun():boolean
 ---@field thread_id? string
 
+---@doc.type
 ---@alias senpai.Config.prompt_launchers table<string, senpai.Config.PromptLauncher.launcher>
 
+---@doc.type
 ---@class senpai.Config.PromptLauncher.resolved_launcher: senpai.ChatWindowNewArgs
 ---@field user_prompt? string
 
